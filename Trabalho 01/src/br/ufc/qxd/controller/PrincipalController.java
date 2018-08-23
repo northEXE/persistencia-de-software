03d8266b5012c98713d97de34dfcc0679f80ba5b
Wendel Ferreira de Mesquita <wendel.ferreira.wfm@hotmail.com>
2018-08-23 01:27:15 -0300

    Serviços de cliente prontos, interface de controle de cliente, pronto

--- Trabalho 01/src/br/ufc/qxd/controller/PrincipalController.java
+++ Trabalho 01/src/br/ufc/qxd/controller/PrincipalController.java
@@ -1,12 +1,40 @@
 package br.ufc.qxd.controller;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import br.ufc.qxd.service.ClienteService;
 
 public class PrincipalController {
 	public static void main(String[] args) throws IOException {
 		ClienteService clienteService = new ClienteService();
-		clienteService.adicionarCliente();
+
+		System.out.println("Livraria do Yoda!! A melhor livraria de todas!!");
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		byte op = -3;
+		while (op != 0) {
+			System.out.println("01 - Serviços de cliente; 02 - Livros; 00 - Sair");
+			switch (op = s.nextByte()) {
+			case 1:
+				while (op != -1) {
+					System.out.println("Opções de serviços cliente:\n" + "01 - Cadastrar Cliente;\n" + "02 - Listar Clientes;\n"
+							+ "03 - Excluir Clientes\n" + "-01 - Voltar;\n");
+					switch (op = s.nextByte()) {
+					case 1:
+						clienteService.adicionarCliente();
+						break;
+					case 2:
+						clienteService.listarClientes();
+						break;
+					case 3:
+						clienteService.removerCliente();
+						break;
+					}
+				}
+				break;
+			}
+		}
+		System.exit(0);
 	}
 }
--- Trabalho 01/src/br/ufc/qxd/service/ClienteService.java
+++ Trabalho 01/src/br/ufc/qxd/service/ClienteService.java
@@ -1,9 +1,15 @@
 package br.ufc.qxd.service;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import br.ufc.qxd.model.Cliente;
@@ -15,8 +21,9 @@ public class ClienteService {
 
 	public void adicionarCliente() throws IOException {
 		System.out.println("Cadastro de Cliente!!\n");
-		Cliente cliente = new Cliente();
 		System.out.println("Digite o nome, CPF, data de nascimento e endereço, respectivamente:\n");
+		
+		Cliente cliente = new Cliente();
 		cliente.setNome(s.nextLine());
 		cliente.setCpf(s.nextLine());
 		cliente.setDataNascimento(s.nextLine());
@@ -26,7 +33,48 @@ public class ClienteService {
 		pw.append(cliente.getNome() + "," + cliente.getCpf() + "," + cliente.getDataNascimento() + ","
 				+ cliente.getEndereco() + ";\n");
 		pw.close();
+
+		System.out.println("Cliente Cadastrado com Sucesso!!\n");
+	}
+
+	public void listarClientes() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(ARQUIVO)));
+		String s = br.readLine();
 		
-		System.out.println("Cliente Cadastrado com Sucesso!!");
+		while (s != null) {
+			System.out.println(s);
+			s = br.readLine();
+		}
+		
+		System.out.println("\n");
+		
+		br.close();
+	}
+
+	public void removerCliente() throws IOException {
+		ArrayList<String> aux = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(ARQUIVO)));
+		
+		System.out.println("Digite os dados do cliente que deseja remover:\n");
+		String clienteApagar = s.nextLine();
+		String linha = br.readLine();
+		
+		while (linha != null) {
+			if (linha.equals(clienteApagar) == false) {
+				aux.add(linha);
+			}
+			linha = br.readLine();
+		}
+		
+		br.close();
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ARQUIVO)));
+		for(String a : aux) {
+			bw.append(a + "\n");
+		}
+		
+		System.out.println("Cliente removido com sucesso!!\n");
+		
+		bw.close();
 	}
 }
